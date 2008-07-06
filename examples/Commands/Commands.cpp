80aaa037fa5718620243ade27ad53282e9e46e93
David Capello <davidcapello@gmail.com>
2008-07-06 21:32:28 +0000

    Added Commands example

---
+++ examples/Commands/Commands.cpp
@@ -0,0 +1,167 @@
+// Vaca - Visual Application Components Abstraction
+// Copyright (c) 2005, 2006, 2007, David A. Capello
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in
+//   the documentation and/or other materials provided with the
+//   distribution.
+// * Neither the name of the Vaca nor the names of its contributors
+//   may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <Vaca/Vaca.hpp>
+
+using namespace Vaca;
+
+//////////////////////////////////////////////////////////////////////
+
+class Console : public MultilineEdit
+{
+  Font m_font;
+  
+public:
+
+  Console(Widget *parent)
+    : MultilineEdit("", parent, MultilineEditStyle + ScrollStyle)
+    , m_font("Courier New", 10)
+  {
+    setFont(&m_font);
+    setBgColor(Color::Black);
+    setFgColor(Color(0, 220, 0));
+  }
+
+  void println(const String &str)
+  {
+    setText(getText() + str + "\r\n");
+    scrollLines(getLineCount());
+  }
+  
+};
+
+//////////////////////////////////////////////////////////////////////
+
+// class MyCommand : public Command // class used by method 2
+// {
+//   Console *m_console;
+
+// public:
+
+//   MyCommand(int id, Console *console)
+//     : Command(id) 
+//     , m_console(console)
+//   {
+//   }
+
+//   virtual void onAction()
+//   {
+//     m_console->println("MyCommand::onAction(" +
+// 		       String::fromInt(getId()) + ")");
+//   }
+
+// };
+
+//////////////////////////////////////////////////////////////////////
+
+class MainFrame : public Frame
+{
+  Console m_console;
+
+public:
+
+  MainFrame()
+    : Frame("Alternatives to hook a command")
+    , m_console(this)
+  {
+    setLayout(new ClientLayout);
+    setMenuBar(new MenuBar);
+
+//     // method 1: Hook the command in Command::Action signal
+//     Command *command;
+//     command = new Command(1000);
+//     command->Action.connect(Bind(&MainFrame::onCommandSlot, this, command));
+//     addCommand(command);
+
+    // method 2: Hook the command in MyCommand::onAction event
+//     addCommand(new MyCommand(1001, &m_console));
+
+    // method 3: Hook the command in MenuItem::Action signal
+    MenuItem *menuItem;
+    menuItem = new MenuItem("&Cmd1(Ctrl+1)",
+			    Keys::Control | Keys::D1,
+			    1000);
+
+    menuItem->Action.connect(&MainFrame::onMenuItemSlot, this);
+    getMenuBar()->add(menuItem);
+
+    menuItem = new MenuItem("&Cmd2(Ctrl+2)",
+			    Keys::Control | Keys::D2,
+			    1001);
+
+    menuItem->Action.connect(&MainFrame::onMenuItemSlot, this);
+    getMenuBar()->add(menuItem);
+  }
+  
+
+protected:
+
+  // method 4: Hook the command in onActionById
+  virtual bool onActionById(int actionId)
+  {
+    // print a message about the execution of this method
+    m_console.println("MainFrame::onActionById("+String::fromInt(actionId)+")");
+
+    // Frame::onActionById() does the MenuItems and Commands stuff
+    Frame::onActionById(actionId);
+
+    // print a separator
+    m_console.println("");
+    return true;
+  }
+
+  // routine used by method 3
+  void onMenuItemSlot(MenuItemEvent &ev)
+  {
+    m_console.println("MainFrame::onMenuItemSlot("+
+		      String::fromInt(ev.getMenuItem()->getId())+")");
+  }
+
+//   // routine used by method 1
+//   void onCommandSlot(Command *cmd)
+//   {
+//     m_console.println("MainFrame::onCommandSlot("+
+// 		     String::fromInt(cmd->getId())+")");
+//   }
+
+};
+
+//////////////////////////////////////////////////////////////////////
+
+int PASCAL WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+		   LPSTR lpCmdLine, int nCmdShow)
+{
+  Application app;
+  MainFrame mainFrame;
+  mainFrame.setVisible(true);
+  app.run();
+  return 0;
+}

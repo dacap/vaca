a5a03a82386216166a2c58d5e6ca773bfb2820f8
David Capello <davidcapello@gmail.com>
2009-02-21 14:19:44 +0000

    Updated copyright years.

--- include/Vaca/Anchor.h
+++ include/Vaca/Anchor.h
@@ -1,5 +1,5 @@
 // Vaca - Visual Application Components Abstraction
-// Copyright (c) 2005, 2006, 2007, 2008, 2009, David Capello
+// Copyright (c) 2005-2009 David Capello
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -38,12 +38,11 @@
 
 namespace Vaca {
 
-/**
- * Anchor constraint. You should setup an Anchor contraint for every
- * widget controled by an AnchorLayout.
- *
- * @see AnchorLayout.
- */
+/// Anchor constraint. You should setup an Anchor contraint for every
+/// widget controled by an AnchorLayout.
+///
+/// @see AnchorLayout.
+///
 class VACA_DLL Anchor : public Constraint
 {
   Rect m_refRect;

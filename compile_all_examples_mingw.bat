d4a91f92c60b32b1d3f3c6770339490694ae2b04
David Capello <davidcapello@gmail.com>
2009-03-24 14:32:58 +0000

    Using CMake as building system.

--- compile_all_examples_mingw.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-echo Compiling Vaca Examples with MinGW...
-call compile_mingw.bat examples "RELEASE=1" "ADD_COMPILER_VERSION=1"
--- compile_all_examples_vc.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-echo Compiling Vaca Examples with MSVC...
-call compile_vc.bat examples "RELEASE=1"

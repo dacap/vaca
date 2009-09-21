72bdd7783d938da5a650852926b06fd142027355
David Capello <davidcapello@gmail.com>
2009-09-21 04:26:23 +0000

    Minor changes to platforms page (compilers, gtk, etc.).

--- doc/pages/platforms.h
+++ doc/pages/platforms.h
@@ -12,17 +12,17 @@ namespace Vaca {
 
 Platforms and Compilers:
 @li Vaca is tested on Win2K and WinXP.
-@li Compiled with MSVC 7.1, MSVC 8.0 and MinGW (GCC 3.4.2, 4.3.0 alpha-testing).
+@li Compiled with MSVC 9.0 (2008 Express) and MinGW (GCC 3.4.2 and 4.4) using CMake 2.6-patch 3.
 
 Platforms which we are not thinking to add support for: Win95, Win98 and WinNT4.
 
 
 @section page_platforms_portability Portability
 
-An objetive is to wrap so much the Win32 API that the common user
-should not see or know nothing about it. So this principle could
-be a sign to make Vaca portable. A posible platform to port Vaca
-could be GTK+.
+An aim with Vaca is to wrap so much the Win32 API that the common user
+should not see or know nothing about it. This principle is a sign to
+make Vaca portable. A possible platform to port Vaca in the future
+is GTK+.
 
 */
 

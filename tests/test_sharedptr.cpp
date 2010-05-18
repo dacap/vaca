173247d0306771d8d73c3ab9f8be5fccdf6f415f
David Capello <davidcapello@gmail.com>
2010-05-18 00:45:38 -0300

    Minor modifications in test for SharedPtr class.

--- tests/test_sharedptr.cpp
+++ tests/test_sharedptr.cpp
@@ -15,10 +15,20 @@ public:
 TEST(SharedPtr, Equal)
 {
   SharedPtr<Int> a(new Int(5));
-  SharedPtr<Int> b;
+  SharedPtr<Int> b(new Int(0));
+  SharedPtr<Int> c;
+
   EXPECT_EQ(5, a->value_);
-  EXPECT_TRUE(a != b);
-  b = a;
+  EXPECT_EQ(0, b->value_);
+
+  b->value_ = a->value_;	// Same values
+
+  EXPECT_TRUE(a != b);		// References are different
+  EXPECT_TRUE(b != c);
+
+  b = a;			// Same reference
+
   EXPECT_TRUE(a == b);
+  EXPECT_TRUE(b != c);
   EXPECT_TRUE(&a->value_ == &b->value_); // Same address
 }
